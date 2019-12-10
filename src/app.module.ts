0a4c0200e74cf7dffc55be0230b0108e8de7bf3b
Mohammed Hanaf <hanaf.n.m@gmail.com>
2019-12-10 09:58:49 +0530

    Reading DB name from config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotesModule } from './notes/notes.module';
+import {ConfigService} from './config/config.service';
+
+
+const configService = new ConfigService(`${process.env.NODE_ENV}.env`);
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/gkeepdb1'),
+  imports: [MongooseModule.forRoot(configService.get('DB_NAME')),
     NotesModule,
   ],
   controllers: [AppController],
